7e7f916fe953d250b0794e1e588fe3c5eaff8d03
Marcus Hindsbøl <cph-1054@cphbusiness.dk>
2023-03-10 10:29:52 +0100

    PersonMapper update

--- src/main/java/mappers/PersonMapper.java
+++ src/main/java/mappers/PersonMapper.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Set;
 
 public class PersonMapper {
+    //Hallo
 
     private static EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
 
