d1e666b87b30db1d749f933aa948be9ea1a3391b
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2023-03-10 10:39:22 +0100

    adds marcus methods into main

--- src/main/java/mappers/PersonMapper.java
+++ src/main/java/mappers/PersonMapper.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -63,6 +64,48 @@ public class PersonMapper {
         }
 
         return new PersonDTO();
+    }
+
+
+    public static int getPersonCount(EntityManagerFactory emf){
+        EntityManager em = emf.createEntityManager();
+        try{
+            return (int)em.createQuery("SELECT COUNT(p) FROM Person p JOIN p.hobbies h " +
+                    "WHERE h.name = :hobby").getSingleResult();
+        }finally{
+            em.close();
+        }
+    }
+
+
+    public static List<PersonDTO> findUsersByHobby(String hobbyName, EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
+        String queryString = "SELECT p FROM Person p JOIN p.hobbies h " +
+                "WHERE h.name = :hobbyName";
+        TypedQuery<Person> query = em.createQuery(queryString, Person.class);
+        query.setParameter("hobbyName", hobbyName);
+        List<Person> resultList = query.getResultList();
+        List<PersonDTO> resultDTOList = new ArrayList<>();
+        for (Person person : resultList) {
+            PersonDTO personDTO = new PersonDTO(person);
+            resultDTOList.add(personDTO);
+        }
+        return resultDTOList;
+    }
+
+    public static PersonDTO editPerson(PersonDTO personDTO, EntityManagerFactory emf ){
+        EntityManager em = emf.createEntityManager();
+        Person person = em.find(Person.class, personDTO.getId());
+        try {
+            em.getTransaction().begin();
+            person.setFirstName(personDTO.getFirstName());
+            person.setLastName(personDTO.getLastName());
+            person.setEmail(personDTO.getEmail());
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+        return new PersonDTO(person);
     }
 
 }
